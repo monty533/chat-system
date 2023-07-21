6ce6a51625d6eea45eefe6844d24cd581f747e0a
Nitin Kumar <70315471+monty533@users.noreply.github.com>
2023-07-21 21:50:09 +0530

    Update settings.py

--- socialapp/socialapp/settings.py
+++ socialapp/socialapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f5aasm!rlqyxf__+fc@7+wtto0(-#wp507^0sekldt)9ymc1p0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,18 +83,18 @@ ASGI_APPLICATION = 'socialapp.asgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sortSolutions',
-        'USER': 'monty',
-        'PASSWORD': 'monty@123',
-        'HOST': '127.0.0.1',
-        'PORT' : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'sortSolutions',
+    #    'USER': 'monty',
+    #    'PASSWORD': 'monty@123',
+    #    'HOST': '127.0.0.1',
+    #    'PORT' : '3306',
+    #}
 }
 
 
